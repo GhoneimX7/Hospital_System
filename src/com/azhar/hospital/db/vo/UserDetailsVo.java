0d6465ff695b5836d6a2b56b459dd46048fd9da9
root <14momagdi@gmail.com>
2021-07-30 11:29:00 +0200

    Video 11: UserDetailsVo Class added!

---
+++ src/com/azhar/hospital/db/vo/UserDetailsVo.java
@@ -0,0 +1,54 @@
+package com.azhar.hospital.db.vo;
+
+/**
+ *
+ * @author fastox
+ */
+public class UserDetailsVo {
+    private int id;
+    private UsersVo usersVo;
+    private String firstName;
+    private String fatherName;
+    private String mobile;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public UsersVo getUsersVo() {
+        return usersVo;
+    }
+
+    public void setUsersVo(UsersVo usersVo) {
+        this.usersVo = usersVo;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+    
+}
